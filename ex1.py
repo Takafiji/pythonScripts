5b350a967cc6788c4bfb2765559807831ded2b27
Takafiji <rossb237@gmail.com>
2012-08-12 08:10:26 -0600

    first commit

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#print "Hello Wrold!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print "I like to poop"
+#print 'I love to poop'
+print 'Sex is good'
+print '"#" (hash tags) are a good way to start comments!'
+#print "whats a # do?"
---
+++ ex3.py
@@ -0,0 +1,55 @@
+# -*- codding: utf-8 -*-
+
+#prints string
+print 'I will now count my chickens:'
+
+#calculates and prints number of 'Hens' using floating point arithmetic
+#30/6=5 25+5=30
+print 'Hens', float(25+30/6)
+
+#calculates and prints number of 'Roosters' using floating point arithmetic
+#25*3=75%4=3 100-3=97
+print 'Roosters', float(100-25*3%4)
+
+#prints string
+print 'Now I will count the eggs:'
+
+#calculates and prints number of 'eggs' using floating point arithmetic
+
+#REMEMBER TO ADD PARANTHISIS!
+#3+2+1-5+(4%2)*(-1/4)+6 
+#(0)*(-1)=0
+#6-5+0+6=7
+print float(3+2+1-5+4%2-1/4+6)
+
+#prints string 
+print 'Is it that true that 3 + 2 < 5 - 7?'
+
+#calculates using floats and checks if 3+2=5 is < (less than) 5-7=-2
+#statement prints false or 0.0 using float 
+print float(3+2<5-7)
+
+#calculates and prints what 3+2 is (5.0) using floating point arithmetic
+print 'What is 3 + 2?', float(3+2)
+
+#calculates and prints what 5-7 is (-2.0) using floating point arithmetic
+print 'What is 5 - 7?', float(5-7)
+
+#prints string
+print "Oh, that's why it's False."
+
+#prints string
+print 'How about some more.'
+
+#checks if 5 is greater (>) than 2 using floating point arithmetic
+#prints true or 1.0 using floats
+print 'Is it greater?', float(5>-2)
+
+#checks if 5 is greater than or equal to (>=) -2 using floating point arithmetic
+#prints true or 1.0 using floats
+print 'Is it greater or equal?', float(5>=-2)
+
+#checks if 5 is less than or equal to (<=) -2 using floating point arithmetic
+#prints false or 0.0 using floats
+print 'Is it less or equal?', float(5<=-2)
+
