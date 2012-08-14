932faea2c77cb57af62165971928b5297b7cbd32
Takafiji <rossb237@gmail.com>
2012-08-13 22:58:18 -0600

    first commit

---
+++ cigCounter.py
@@ -0,0 +1,11 @@
+#calculates cigarettes smoked, sees how much they're killing you
+
+cigsAWeek = float(((3+4)/2)*7)
+cigsWeekOne = float(23)
+
+print 'Welcome, hope all is well\n'
+print 'Average number of cigarettes a day this week.', cigsAWeek
+
+print 'Is this more than last week.', cigsAWeek > cigsWeekOne
+
+
